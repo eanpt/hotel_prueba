9e6c738409503c33452c80a6041c50afc870f018
Nikodev777 <nikolasm079@gmail.com>
2024-11-09 21:46:13 -0500

    modifique el css

---
+++ asset/script.js
@@ -0,0 +1,29 @@
+let boton = document.getElementById("miBoton");
+
+boton.addEventListener("click", function() {
+    // Redirigir a otra página (por ejemplo, Google)
+    window.location.href = "https://www.google.com"; });
+  
+    let boton2 = document.getElementById("miBoton2");
+
+boton.addEventListener("click", function() {
+    // Redirigir a otra página (por ejemplo, Google)
+    window.location.href = "https://www.google.com"; });
+
+    let boton3 = document.getElementById("miBoton3");
+
+boton.addEventListener("click", function() {
+    // Redirigir a otra página (por ejemplo, Google)
+    window.location.href = "https://www.google.com"; });
+
+    let boton4 = document.getElementById("miBoton4");
+
+boton.addEventListener("click", function() {
+    // Redirigir a otra página (por ejemplo, Google)
+    window.location.href = "https://www.google.com"; });
+
+    let boton5 = document.getElementById("miBoton5");
+
+    boton.addEventListener("click", function() {
+        // Redirigir a otra página (por ejemplo, Google)
+        window.location.href = "https://www.google.com"; });
