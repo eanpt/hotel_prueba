1d53957b91e18b266bfb7a8eef77b650a495c81c
Nikodev777 <nikolasm079@gmail.com>
2024-11-10 20:14:56 -0500

    agregue botones

--- asset/script.js
+++ asset/script.js
@@ -6,24 +6,24 @@ boton.addEventListener("click", function() {
   
     let boton2 = document.getElementById("miBoton2");
 
-boton.addEventListener("click", function() {
+boton2.addEventListener("click", function() {
     // Redirigir a otra página (por ejemplo, Google)
     window.location.href = "https://www.google.com"; });
 
     let boton3 = document.getElementById("miBoton3");
 
-boton.addEventListener("click", function() {
+boton3.addEventListener("click", function() {
     // Redirigir a otra página (por ejemplo, Google)
     window.location.href = "https://www.google.com"; });
 
     let boton4 = document.getElementById("miBoton4");
 
-boton.addEventListener("click", function() {
+boton4.addEventListener("click", function() {
     // Redirigir a otra página (por ejemplo, Google)
     window.location.href = "https://www.google.com"; });
 
     let boton5 = document.getElementById("miBoton5");
 
-    boton.addEventListener("click", function() {
+    boton5.addEventListener("click", function() {
         // Redirigir a otra página (por ejemplo, Google)
         window.location.href = "https://www.google.com"; });
